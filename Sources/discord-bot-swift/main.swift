cb6197b2cf6d75a682f7eb6d4bdd1058808574be
teddy <teddyjh@protonmail.com>
2022-05-24 09:55:57 +0200

    add two new bot commands: !marko & !xkcd
    !marko : answers the caller with "polo!"
    !xkcd : answers the caller with "help" right now, will send xkcd comic
    img later

--- Sources/discord-bot-swift/main.swift
+++ Sources/discord-bot-swift/main.swift
@@ -1,14 +1,17 @@
 import Sword
-import token.swift
 
 let bot = Sword(token: getBotToken())
 
 bot.on(.messageCreate) { data in 
     let msg = data as! Message
 
-    if msg.content == "!ping" {
-        msg.reply(with: "pong!")
+    if msg.content == "!marko" || msg.content == "!marco" {
+        msg.reply(with: "polo!")
+    } else if msg.content == "!xkcd" {
+        msg.reply(with: xkcd())
     }
+    
+    
 }
 
 bot.connect()
