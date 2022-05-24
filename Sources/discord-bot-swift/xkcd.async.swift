009f89a8d424cb1d112f99e2b93bdc898b4238b8
teddy <teddyjh@protonmail.com>
2022-05-24 16:27:11 +0200

    add new file for async-await xkcd-command

---
+++ Sources/discord-bot-swift/xkcd.async.swift
@@ -0,0 +1,12 @@
+//
+//  xkcd.async.swift
+//  
+//
+//  Created by teddy juhlin-henricson on 2022-05-24.
+//
+
+import Foundation
+
+// [] add xkcd() async func
+// [] add xkcd-api async fetch
+// [] add json-parser
