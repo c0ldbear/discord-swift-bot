8b3a6aaf2612c7106dd68f41142e74909e47c77d
teddy <teddyjh@protonmail.com>
2022-05-25 10:09:10 +0200

    add 'async-await' approach to get XKCD comic
    Add an additional approach using 'async-await' to fetch the XKCD json
    using their API.
    Change the bot command '!xkcd' to use the 'async-await' approach first,
    but if the OS version of where the bot is running is lower than macOS 12
    then there is a fallback using completion. Subject line (try to keep under 50 characters)

--- Sources/discord-bot-swift/main.swift
+++ Sources/discord-bot-swift/main.swift
@@ -8,8 +8,17 @@ bot.on(.messageCreate) { data in
     if msg.content == "!marko" || msg.content == "!marco" {
         msg.reply(with: "polo!")
     } else if msg.content == "!xkcd" {
-        xkcd() { inMsg in
-            msg.reply(with: inMsg)
+        if #available(macOS 12, *) {
+            print("Using 'async-await' to fetch XKCD comic.")
+            Task.init {
+                msg.reply(with: await xkcd())
+            }
+        } else {
+            // Fallback on earlier versions
+            print("Using 'completion' to fetch XKCD comic.")
+            xkcd() { inMsg in
+                msg.reply(with: inMsg)
+            }
         }
     }
 }
--- Sources/discord-bot-swift/xkcd.async.swift
+++ Sources/discord-bot-swift/xkcd.async.swift
@@ -7,6 +7,47 @@
 
 import Foundation
 
-// [] add xkcd() async func
-// [] add xkcd-api async fetch
-// [] add json-parser
+// Will use XkcdData struct from xkcd.swift
+
+@available(macOS 12.0, *)
+func xkcd() async -> String {
+    let xkcdUrl = "https://xkcd.com/info.0.json"
+    return await loadJson(fromURLString: xkcdUrl)
+}
+
+@available(macOS 12.0, *)
+private func loadJson(fromURLString urlString: String) async -> String {
+    
+    if let url = URL(string: urlString) {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            let httpResponse = response as! HTTPURLResponse
+            if httpResponse.statusCode == 200 {
+                let botMsg = parseJson(data: data)
+                return botMsg
+            } else {
+                return "No response from\(urlString)"
+            }
+        } catch {
+            print("Fetch error? \(error)")
+        }
+    }
+    
+    return urlString
+}
+
+private func parseJson(data: Data) -> String {
+    do {
+        let decodedData: XkcdData = try JSONDecoder().decode(XkcdData.self, from: data)
+        let repsonseMessage = """
+            \(decodedData.title)
+            <spoilers>||\(decodedData.alt)||</spoilers>
+            \(decodedData.img)
+            
+            """
+        return repsonseMessage
+    } catch {
+        print("Decoding JSON error: \(error)")
+    }
+    return "JSON Parse Error"
+}
