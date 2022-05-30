de5f56aac7b4c8c2eecbabda0b07d7161e963667
teddy <teddyjh@protonmail.com>
2022-05-30 13:54:58 +0200

    refactor to functions: find articles urls & create bot msg

--- Sources/discord-bot-swift/dn.news.swift
+++ Sources/discord-bot-swift/dn.news.swift
@@ -5,14 +5,21 @@
 //  Created by teddy juhlin-henricson on 2022-05-25.
 //
 
+// TODO:
+//  [] Rewrite as a Class
+//  [x] Refactor to seperate functions/method
+
 import Foundation
 import SwiftSoup
 
+private let dnBaseUrl = "https://www.dn.se"
+private let dnNyhetsDygnetUrl = dnBaseUrl + "/nyhetsdygnet/"
+
 @available(macOS 12.0, *)
 func dn() async -> String {
-    let dnBaseUrl = "https://www.dn.se"
-    let dnNyhetsDygnetUrl = dnBaseUrl + "/nyhetsdygnet/"
-    var dnNewsLinksMsg: String = "Dagens senaste nyheter\n\n"
+    
+    // Properties
+//    var dnNewsLinksMsg: String = "Dagens senaste nyheter\n\n"
     
     if let url = URL(string: dnNyhetsDygnetUrl) {
         do {
@@ -20,17 +27,11 @@ func dn() async -> String {
             let httpResponse = response as! HTTPURLResponse // (Forced) type casting
             if httpResponse.statusCode == 200 {
                 let html = String(data: data, encoding: .utf8)!
-                let doc: Document = try SwiftSoup.parse(html)
-                let aHrefs = try doc.select("a.timeline-teaser")
                 
-                for link: Element in aHrefs.array()[..<5] {
-                    let articleLink: String = try link.attr("href")
-                    print("text? \(articleLink), type? \(type(of: articleLink))\n")
-                    dnNewsLinksMsg += dnBaseUrl + articleLink + "\n"
-                }
+                let newsArticleLinks = try findNewsArticleLinks(html: html)
                 
-                dnNewsLinksMsg += "\n\n" + dnNyhetsDygnetUrl + "\n"
-                print("dn news links msg: \(dnNewsLinksMsg)")
+                let dnNewsLinksMsg = try createBotMsg(newsArticleLinks)
+                
                 return dnNewsLinksMsg
             } else {
                 print("Some other http response code: \(httpResponse.statusCode)")
@@ -40,5 +41,28 @@ func dn() async -> String {
         }
     }
     
-    return "Hej där!"
+    return "Hej där! Funktionen är inte helt implementerad."
+}
+
+private func findNewsArticleLinks(html: String) throws -> Elements {
+    let doc: Document = try SwiftSoup.parse(html)
+    let newsArticleHtmlTagAndCssClass = "a.timeline-teaser" // format: "HtmlTag.CSSClass"
+    let newsArticleLinks: Elements = try doc.select(newsArticleHtmlTagAndCssClass)
+    return newsArticleLinks
+}
+
+private func createBotMsg(_ links: Elements) throws -> String {
+    if !links.isEmpty {
+        var dnNewsLinksMsg: String = "Dagens seanste nyheter\n\n"
+        
+        for link: Element in links.array()[..<5] {
+            let articleLink: String = try link.attr("href")
+//            print("text? \(articleLink), type? \(type(of: articleLink))\n")
+            dnNewsLinksMsg += dnBaseUrl + articleLink + "\n"
+        }
+        
+        dnNewsLinksMsg += "\n\n" + dnNyhetsDygnetUrl + "\n"
+        return dnNewsLinksMsg
+    }
+    return "Something something something lol"
 }
